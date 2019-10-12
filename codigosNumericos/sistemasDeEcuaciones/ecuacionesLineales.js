6b5f9eff3453afcce13bc7c79c8d3d797fda3d68
Mateo RH <ramirezmateorh@gmail.com>
2019-10-12 10:15:20 -0500

    correccion doolittle

--- codigosNumericos/sistemasDeEcuaciones/ecuacionesLineales.js
+++ codigosNumericos/sistemasDeEcuaciones/ecuacionesLineales.js
@@ -1,6 +1,6 @@
 const math = require('mathjs');
 
-let m7 = [[36, 3, -4, 5], [5, -45, 10, -2], [6, 8, 57, 5], [2, 3, -8, -42]];
+let m7 = [[3, -2, -2, -1], [-2, 8, -2, -2], [-2, -2, 4, 2], [-1, -2, 2, 3]];
 let m8 = [[36, 3, -4, 5], [5, -45, 10, -2], [6, 8, 57, 5], [2, 3, -8, -42]];
 
 let factorizacionDirectaCrout = matrix => {
@@ -14,7 +14,6 @@ let factorizacionDirectaCrout = matrix => {
 
   for (let k = 0; k < n; k++) {
     var suma1 = 0;
-
     for (let p = 0; p < k; p++) {
       suma1 += L[k][p] * U[p][k];
     }
@@ -37,8 +36,10 @@ let factorizacionDirectaCrout = matrix => {
     }
   }
 
-  console.log(L);
-  console.log(U);
+  console.log('L');
+  console.table(L);
+  console.log('U');
+  console.table(U);
 };
 
 let factorizacionDirectaDoolittle = matrix => {
@@ -53,30 +54,26 @@ let factorizacionDirectaDoolittle = matrix => {
   for (let k = 0; k < n; k++) {
     var suma1 = 0;
 
-    for (let p = 0; p < k; p++) {
-      suma1 += L[k][p] * U[p][k];
-    }
+    for (let p = 0; p < k; p++) suma1 += L[k][p] * U[p][k];
     U[k][k] = matrix[k][k] - suma1;
 
     for (let i = k + 1; i < n; i++) {
       var suma2 = 0;
-      for (let p = 0; p < k; p++) {
-        suma2 += L[i][p] * U[p][k];
-      }
-      U[i][k] = (matrix[i][k] - suma2) / L[k][k];
+      for (let p = 0; p < k; p++) suma2 += L[i][p] * U[p][k];
+      U[k][i] = (matrix[k][i] - suma2) / L[k][k];
     }
 
     for (let j = k + 1; j < n; j++) {
       var suma3 = 0;
-      for (let p = 0; p < k; p++) {
-        suma3 += L[k][p] * U[p][j];
-      }
-      L[k][j] = (matrix[k][j] - suma3) / U[k][k];
+      for (let p = 0; p < k; p++) suma3 += L[k][p] * U[p][j];
+      L[j][k] = (matrix[j][k] - suma3) / U[k][k];
     }
   }
 
-  console.log(L);
-  console.log(U);
+  console.log('L');
+  console.table(L);
+  console.log('U');
+  console.table(U);
 };
 
 console.log('CROUT');
