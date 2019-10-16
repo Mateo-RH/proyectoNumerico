21d7933b7ab3452f36dc827e58c9239503a8623a
MateoRH <marami26@eafit.edu.co>
2019-10-16 11:28:31 -0500

    Añadiendo gaussSeidel

--- codigosNumericos/sistemasDeEcuaciones/iterativos/gaussSeidel.js
+++ codigosNumericos/sistemasDeEcuaciones/iterativos/gaussSeidel.js
@@ -1,15 +1,17 @@
 const math = require('mathjs');
 
+// TODO: SOLUCION POR ECUACION, CORREGIR (NO TOCAR EN EL QUIZ)
+// ====================INICIO==================================
 let gaussSeidel = (tolerancia, vector0, niter, matrix, b) => {
   var dispercion = tolerancia + 1;
   var counter = 1;
   while (dispercion > tolerancia && counter <= niter) {
     var x1 = calcularNuevogaussSeidel(vector0, matrix, b);
-    // FIXME: Dispercion de mier
-    dispercion = math.max(x1.map((item, index) => item - vector0[index]));
+    dispercion = math.max(math.abs(math.subtract(x1, vector0)));
     vector0 = x1;
     counter++;
   }
+  console.log(x1);
   if (dispercion < tolerancia) {
     console.log(`${x1} es aproximacion con una tolerancia = ${tolerancia}`);
   } else {
@@ -30,8 +32,64 @@ let calcularNuevogaussSeidel = (vector0, matrix, b) => {
   return vector1;
 };
 
-let m = [[8, 3, 5], [-2, 7, 3], [4, -5, 18]];
+// ===================FIN====================================
 
-let b = [21, 7, 42];
+let getLDU = matrix => {
+  let L = matrix.map((fila, filIdx) =>
+    fila.map((columna, colIdx) => {
+      return colIdx < filIdx ? -columna : 0;
+    })
+  );
+  let D = matrix.map((fila, filIdx) =>
+    fila.map((columna, colIdx) => {
+      return colIdx == filIdx ? columna : 0;
+    })
+  );
+  let U = matrix.map((fila, filIdx) =>
+    fila.map((columna, colIdx) => {
+      return colIdx > filIdx ? -columna : 0;
+    })
+  );
+  return { L, D, U };
+};
 
-calcularNuevogaussSeidel(0.0000001, [0, 0, 0], 5, m, b);
+// w = 1, gauss seidel
+// 0<w<1, subrelajacion (para converger subsistemas que no convergen por gauss seidel)
+// 1<w<2, sobre-relajacion(aceleran convergencia en gauss seidel convergente lento)
+let matrixSeidel = (tolerancia, x0, niter, matrix, b, w = 1) => {
+  let { L, D, U } = getLDU(matrix);
+
+  let temp = math.subtract(D, math.multiply(L, w));
+  let temp2 = math.add(math.multiply(1 - w, D), math.multiply(w, U));
+  let T = math.multiply(math.inv(temp), temp2);
+  let C = math.multiply(w, math.multiply(math.inv(temp), b));
+
+  var err = tolerancia + 1;
+  var counter = 0;
+  var tabla = [];
+
+  while (err > tolerancia && counter < niter) {
+    var x1 = math.add(math.multiply(T, x0), C);
+    err = norma(x1, x0);
+    x0 = x1;
+    counter++;
+    tabla.push({ xn: x1, err });
+  }
+
+  console.table(tabla, ['xn', 'err']);
+  if (err < tolerancia) {
+    console.log(`${x1} es aproximacion con una tolerancia = ${tolerancia}`);
+  } else {
+    console.log(`fracaso en ${niter} iteraciones`);
+  }
+};
+
+let norma = (x1, x0) => {
+  return math.max(math.abs(math.subtract(x1, x0)));
+};
+
+let m = [[5, 3, 1], [3, 4, -1], [1, -1, 4]];
+
+let b = [24, 30, -24];
+
+matrixSeidel(0.00000001, [0, 0, 0], 30, m, b);
--- codigosNumericos/sistemasDeEcuaciones/iterativos/jacobi.js
+++ codigosNumericos/sistemasDeEcuaciones/iterativos/jacobi.js
@@ -2,14 +2,16 @@ const math = require('mathjs');
 
 let jacobi = (tolerancia, vector0, niter, matrix, b) => {
   var dispercion = tolerancia + 1;
-  var counter = 1;
-  while (dispercion > tolerancia && counter <= niter) {
+  var counter = 0;
+  var tabla = [];
+  while (dispercion > tolerancia && counter < niter) {
     var x1 = calcularNuevoJacobi(vector0, matrix, b);
-    // FIXME: Dispercion de mier
-    dispercion = math.max(x1.map((item, index) => item - vector0[index]));
+    dispercion = math.max(math.abs(math.subtract(x1, vector0)));
     vector0 = x1;
     counter++;
+    tabla.push({ xn: x1, err: dispercion });
   }
+  console.table(tabla, ['xn', 'err']);
   if (dispercion < tolerancia) {
     console.log(`${x1} es aproximacion con una tolerancia = ${tolerancia}`);
   } else {
@@ -30,8 +32,57 @@ let calcularNuevoJacobi = (vector0, matrix, b) => {
   return vector1;
 };
 
-let m = [[8, 3, 5], [-2, 7, 3], [4, -5, 18]];
+// TODO: SOLUCION POR MATRIX, CORREGIR (NO TOCAR EN EL QUIZ)
+// ====================INICIO==================================
+let getLDU = matrix => {
+  let L = matrix.map((fila, filIdx) =>
+    fila.map((columna, colIdx) => {
+      return colIdx < filIdx ? columna : 0;
+    })
+  );
+  let D = matrix.map((fila, filIdx) =>
+    fila.map((columna, colIdx) => {
+      return colIdx == filIdx ? columna : 0;
+    })
+  );
+  let U = matrix.map((fila, filIdx) =>
+    fila.map((columna, colIdx) => {
+      return colIdx > filIdx ? columna : 0;
+    })
+  );
+  return { L, D, U };
+};
 
+let matrixJacobi = (tolerancia, x0, niter, matrix, b) => {
+  let { L, D, U } = getLDU(matrix);
+  console.table(L);
+  console.table(D);
+  console.table(U);
+  let T = math.multiply(math.inv(D), math.add(L, U));
+  let C = math.multiply(math.inv(D), b);
+  console.table(T);
+  console.table(C);
+  var err = tolerancia + 1;
+  var counter = 1;
+
+  while (err > tolerancia && counter <= niter) {
+    var x1 = math.add(math.multiply(T, x0), C);
+    err = math.max(math.abs(math.subtract(x1, x0)));
+    x0 = x1;
+    counter++;
+  }
+
+  console.log(x1);
+  if (err < tolerancia) {
+    console.log(`${x1} es aproximacion con una tolerancia = ${tolerancia}`);
+  } else {
+    console.log(`fracaso en ${niter} iteraciones`);
+  }
+};
+// ===================FIN====================================
+
+let m = [[8, 3, 5], [-2, 7, 3], [4, -5, 18]];
 let b = [21, 7, 42];
 
 jacobi(0.0000001, [0, 0, 0], 5, m, b);
+// matrixJacobi(0.0000001, [0, 0, 0], 5, m, b);
