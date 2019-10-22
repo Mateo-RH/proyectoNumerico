6c0dff7c68efd4f409bbf994d92d9c02a856cc6d
MateoRH <marami26@eafit.edu.co>
2019-10-22 13:01:36 -0500

    Corrigiendo errores en simplificacion de ecuaciones, agregando manejo de errores para vandermonde, newton, lagrange y gaussPivoteoTotal

--- codigosNumericos/interpolacion/index.js
+++ codigosNumericos/interpolacion/index.js
@@ -2,18 +2,14 @@ const { vandermonde } = require('./vandermonde');
 const { newton } = require('./newton');
 const { lagrange } = require('./lagrange');
 
-// const funcion = '(e^x)-6x';
-const funcion = '-9 + 21.09x - 35.16x^2 + 21.95x^3 + 5.333x^4 + 0.44x^5';
-const puntosX = [-9, -6, -3, 3, 5, 9];
+const funcion = '3x^2 - 5';
+const puntosX = [-1, 0, 1, 2];
 const punto = 1;
 
-// const funcion2 = '4x + 1';
-// const puntosX2 = [5, 9];
-
-console.log('VANDERMONDE');
-vandermonde(funcion, puntosX, punto);
-console.log('NEWTON');
-newton(funcion, puntosX, punto);
+// console.log('VANDERMONDE');
+// vandermonde(funcion, puntosX, punto);
+// console.log('NEWTON');
+// newton(funcion, puntosX, punto);
 console.log('LAGRANGE');
 lagrange(funcion, puntosX, punto);
 
--- codigosNumericos/sistemasDeEcuaciones/directos/gaussPivotevoTotal.js
+++ codigosNumericos/sistemasDeEcuaciones/directos/gaussPivotevoTotal.js
@@ -8,7 +8,8 @@ let gaussPivotevoTotal = matrix => {
   for (let k = 0; k < n - 1; k++) {
     matrix = pivoteoTotal(matrix, k, marcas);
     for (let i = k + 1; i < n; i++) {
-      let multiplicador = matrix[i][k] / matrix[k][k];
+      if (matrix[i][k] == 0 && matrix[k][k] == 0) return false;
+      var multiplicador = matrix[i][k] / matrix[k][k];
       for (let j = k; j < n + 1; j++) {
         matrix[i][j] = matrix[i][j] - multiplicador * matrix[k][j];
       }
