555cde52fa50412e56a402884e8e972402e9f013
Mateo RH <ramirezmateorh@gmail.com>
2019-10-23 10:37:24 -0500

    MERGE

--- codigosNumericos/interpolacion/index.js
+++ codigosNumericos/interpolacion/index.js
@@ -2,18 +2,6 @@ const { vandermonde } = require('./vandermonde');
 const { newton } = require('./newton');
 const { lagrange } = require('./lagrange');
 
-<<<<<<< HEAD
-const funcion = '-9 + 11.1x - 15.67x^2 + 9.71x^3 + 2.33x^4 + 0.19x^5';
-const puntosX = [-9, -6, -3, 3, 5, 9];
-const punto = 1;
-
-console.log('VANDERMONDE');
-vandermonde(funcion, puntosX, punto);
-console.log('NEWTON');
-newton(funcion, puntosX, punto);
-// console.log('LAGRANGE');
-// lagrange(funcion, puntosX, punto);
-=======
 const funcion = '3x^2 - 5';
 const puntosX = [-1, 0, 1, 2];
 const punto = 1;
@@ -24,6 +12,5 @@ const punto = 1;
 // newton(funcion, puntosX, punto);
 console.log('LAGRANGE');
 lagrange(funcion, puntosX, punto);
->>>>>>> refs/remotes/origin/master
 
 module.exports = { vandermonde, newton, lagrange };
