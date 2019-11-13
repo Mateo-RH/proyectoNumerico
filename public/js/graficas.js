264182296b50f11a20551e2730b12e6f89dd0856
Juan Sebastian Gomez <gomex086@gmail.com>
2019-11-13 16:12:40 -0500

    quickfix to euler again

--- public/js/graficas.js
+++ public/js/graficas.js
@@ -59,11 +59,6 @@ function add() {
     alert('Please enter a function');
     return;
   }
-  funcionF = ' ' + funcionF;
-  funcionF = funcionF.replace('e^', 'exp');
-  funcionF = funcionF.replace(' e ', 'exp(x)');
-  funcionF = funcionF.replace('e ', 'exp(x)');
-  funcionF = funcionF.replace(' e', 'exp(x)');
 
   guardarFuncion(funcionF);
   graficar();
